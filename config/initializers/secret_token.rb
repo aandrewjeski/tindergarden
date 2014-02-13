860ab97be4f7f34f2945c2e5fcb858f1cc3c704d
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-02-12 22:06:57 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tindergarden::Application.config.secret_key_base = '6b242d678fbc6154676c75a0408c012b192ffa8a5d107c1a7598487715cde596e585e5a2b9410d9e11b65db26aa82fb223cc9fc8e2c16e232189c17b845f2643'
